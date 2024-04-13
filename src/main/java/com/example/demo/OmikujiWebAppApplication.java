4f1ed8a82c4bafbb0b2b5bc86cd5085eb9cf708e
寺澤徳隆 <mosqey1889@gmail.com>
2024-04-14 08:39:06 +0900

    FirstCommit

---
+++ src/main/java/com/example/demo/OmikujiWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmikujiWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmikujiWebAppApplication.class, args);
+	}
+
+}
